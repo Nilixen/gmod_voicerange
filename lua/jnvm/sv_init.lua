56a87e68323ab5c3255886ead0b7e3d22f9ecd99
JimPaper <marcin78893@gmail.com>
2021-01-28 14:04:02 +0100

    Kocham cię tak bardzo że aż też tęsknie za tb bardzo

--- lua/jnvm/sv_init.lua
+++ lua/jnvm/sv_init.lua
@@ -1,9 +1,31 @@
+local GlobalVoice = false
+function GAMEMODE:PlayerCanHearPlayersVoice(listener, speaker)
 
-hook.Add( "PlayerCanHearPlayersVoice", "JNVM_VOICE", function( listener, talker )
-  -- 250000 = 500 unit
-  if listener:GetPos():DistToSqr( talker:GetPos() ) > 250000 and talker:Alive() != listener:Alive()
-  or talker:Team() != 1 and talker:KeyDown(IN_SPEED) and talker:Team() != listener:Team() then
-      print("Not")
-      return false
+  if (not speaker:Alive()) and (not listener:Alive()) then
+     return true, false
   end
-end )
+
+  if speaker:KeyDown(IN_SPEED) and speaker:Team() == listener:Team() then
+       return true, false
+  end
+
+   if (not IsValid(speaker)) or (not IsValid(listener)) or (listener == speaker) then
+      return false, false
+   end
+
+   if (not speaker:Alive()) then
+      return false, false
+   end
+
+
+   if listener:GetPos():DistToSqr( speaker:GetPos() ) > 250000 then
+     return false
+   end
+
+
+   if GlobalVoice then
+     return true, false
+   else
+     return true, true
+   end
+end
